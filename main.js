0c93ea6c55ad59b9d784b8b9b9b98b65196ea4dd
Maira Naeem <163671644+mairanoor412@users.noreply.github.com>
2024-05-15 09:21:02 -0700

    assignment3

---
+++ main.js
@@ -0,0 +1,29 @@
+// Friend checker game:
+// • Prompt the user to enter a name.
+// • Use a switch statement to check if the entered name is a known friend.
+// • Output a confirmation message if the name is known, otherwise output a default response.
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "userName",
+        type: "string",
+        message: "Enter your name: "
+    },
+]);
+//let userName = answer
+switch (answer.userName) {
+    case "tooba":
+        console.log("you're know");
+        break;
+    case "zainab":
+        console.log("you're know");
+        break;
+    case "kashaf":
+        console.log("you're know");
+        break;
+    case "zahira":
+        console.log("you're know");
+        break;
+    default:
+        console.log("default response");
+}
